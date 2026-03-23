1b2f3e159b415b2f67c9a29652f36fb5b3883888
Developer <dev@ozsky.com>
2026-03-23 08:04:38 +0800

    優化構建配置以減少內存使用

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
+  swcMinify: true,
+  experimental: {
+    optimizeCss: false,
+  },
 }
 
 module.exports = nextConfig
