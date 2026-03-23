a66ec208271a91d4d819b780dfc7867e9273ae55
Developer <dev@ozsky.com>
2026-03-23 08:09:01 +0800

    降級到 Next.js 13.5.6 以解決構建問題

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
-  swcMinify: true,
-  experimental: {
-    optimizeCss: false,
-  },
 }
 
 module.exports = nextConfig
